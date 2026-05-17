63f004c268453556d14ad0a534db96c8a2bf8e85
Kurosakiniel <ichigoniel@gmail.com>
2026-05-17 15:10:15 -0300

    Mais uma questao superfacil

---
+++ NEPSACADEMY/SuperFacil/43_InteiroDeDoisDigitos.py
@@ -0,0 +1,33 @@
+a = int(input())
+b = int(input())
+
+if a == 1:
+    a = 10
+    print(a + b)
+elif a == 2:
+    a = 20
+    print(a + b)
+elif a == 3:
+    a = 30
+    print(a + b)
+elif a == 3:
+    a = 30
+    print(a + b)
+elif a == 4:
+    a = 40
+    print(a + b)
+elif a == 5:
+    a = 50
+    print(a + b)
+elif a == 6:
+    a = 60
+    print(a + b)
+elif a == 7:
+    a = 70
+    print(a + b)
+elif a == 8:
+    a = 80
+    print(a + b)
+elif a == 9:
+    a = 90
+    print(a + b)
