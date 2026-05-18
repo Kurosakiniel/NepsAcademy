c9293516eb3874780dd28ebc063d6b63ea96d93f
Kurosakiniel <ichigoniel@gmail.com>
2026-05-18 13:41:40 -0300

    mais uma questao superfacil

---
+++ NEPSACADEMY/SuperFacil/45_Pizzaria.py
@@ -0,0 +1,8 @@
+g = int(input())
+p = int(input())
+
+fatiasPizzaGrande = g * 8
+fatiasPizzaPequena = p * 4 
+fatiasLarrisaEMae = 2
+quantidadeDeConvidados = (fatiasPizzaGrande + fatiasPizzaPequena) - fatiasLarrisaEMae
+print(quantidadeDeConvidados)
